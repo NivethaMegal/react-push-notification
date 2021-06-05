37602996aa36b8d54e543f5f077ce3adcc930890
Nivetha R <nivetha@moonraft.com>
2021-06-05 23:18:50 +0530

    notification using packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
-import { requestFirebaseNotificationPermission } from "./firebase";
+// import Notification from 'react-web-notification';
+// import { requestFirebaseNotificationPermission } from "./firebase";
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+// import 'react-notifications/lib/notifications.css';
+import NotificationSystem from 'react-notification-system';
+import addNotification from 'react-push-notification';
+import WebNotification from 'react-web-notifications'
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  notificationSystem = React.createRef();
+
+  addNotification = event => {
+    event.preventDefault();
+    const notification = this.notificationSystem.current;
+    notification.addNotification({
+      message: 'Notification message',
+      level: 'error'
+    });
+  };
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'success':
+          NotificationManager.success('Success message', 'Title here');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+        default:
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+      }
+    };
+  };
+
   componentDidMount() {
     // const messaging = firebase.messaging();
     // Notification.requestPermission().then(function (permission) {
@@ -21,25 +66,84 @@ class App extends Component {
     // });
   }
   render() {
-    requestFirebaseNotificationPermission()
-      .then((firebaseToken) => {
-        // eslint-disable-next-line no-console
-        console.log(firebaseToken);
-      })
-      .catch((err) => {
-        return err;
+    // const showNotification = () => {
+    //   console.log('hello');
+    //   const notify = () => {
+    //     navigator.serviceWorker.ready.then((registration) => {
+    //       registration.showNotification('Adva Missions !! ', {
+    //         body: 'Buzz! Buzz! There is a new message for you...',
+    //         vibrate: [200, 100, 200, 100, 200, 100, 200],
+    //       });
+    //     });
+    //   };
+    //   if (Notification.permission === 'granted') {
+    //     notify();
+    //   } else if (Notification.permission !== 'denied') {
+    //     Notification.requestPermission();
+    //   }
+    //   Notification.requestPermission().then((permission) => {
+    //     if (permission === 'granted') {
+    //       notify();
+    //     }
+    //   });
+    // }
+    // requestFirebaseNotificationPermission()
+    //   .then((firebaseToken) => {
+    //     // eslint-disable-next-line no-console
+    //     console.log(firebaseToken);
+    //   })
+    //   .catch((err) => {
+    //     return err;
+    //   });
+    const buttonClick = () => {
+      addNotification({
+        title: 'Warning',
+        subtitle: 'This is a subtitle',
+        message: 'This is a very long message',
+        theme: 'darkblue',
+        native: true // when using native, your OS will handle theming.
       });
+    };
     return (
       <div>
-        {/* <Notification
-          title="Hi..."
-          options={{
-            body: "There is a new message from adva missions !! stay tuned !!",
-          }}
-          swRegistration={this.props.swRegistration}
-        /> */}
         <h1>Adva Missions...!</h1>
         <p>Welcome You all !!</p>
+        <div>
+          <button className='btn btn-info'
+            onClick={this.createNotification('info')}>Info
+        </button>
+          <hr />
+          <button className='btn btn-success'
+            onClick={this.createNotification('success')}>Success
+        </button>
+          <hr />
+          <button className='btn btn-warning'
+            onClick={this.createNotification('warning')}>Warning
+        </button>
+          <hr />
+          <button className='btn btn-danger'
+            onClick={this.createNotification('error')}>Error
+        </button>
+
+          <NotificationContainer />
+        </div>
+        <div>
+          <button onClick={this.addNotification}>Add notification</button>
+          <NotificationSystem ref={this.notificationSystem} />
+        </div>
+        <button onClick={buttonClick} className="button">
+          Hello world.
+          </button>
+        <WebNotification
+          title="Hello, World!" // the title prop is required
+          icon="path/to/image.jpg"
+          body="This is a web notification"
+          timeout={9000}
+          onClickFn={() => window.open('http://www.google.com/', '_blank')} // open your own site on notification click
+        />
+        {/* <button type='button' onClick={() => showNotification()}>
+          Get Notify !
+      </button> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,26 +3,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { registerServiceWorker } from './service-worker';
+import { Notifications } from 'react-push-notification';
+// import { registerServiceWorker } from './service-worker';
 
 // var mobile = /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(
 //   navigator.userAgent.toLowerCase()
 // );
 // if (mobile) {
-//   navigator.serviceWorker.register("firebase-messaging-sw.js").then(function (registration) {
+//   navigator.serviceWorker.register("sw.js").then(function (registration) {
 //     ReactDOM.render(<App swRegistration={registration} />);
 //   });
 // } else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+//   );
 // }
+ReactDOM.render(
+  <React.StrictMode>
+    <Notifications />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-registerServiceWorker();
+// registerServiceWorker();
